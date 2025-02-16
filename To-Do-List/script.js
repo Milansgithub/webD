914b77e408a6ec295bb9771415df920c24e8e356
Milan <160263928+Milansgithub@users.noreply.github.com>
2025-02-16 23:31:05 +0530

    Update script.js

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -9,7 +9,29 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
-    inputBox.value = '';
+    inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toogle("checked");
+    saveData();
+}  
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+  }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
